1386cc25f68eb56971b318d8276dbc2795ad11b8
Jazmin Matos <jmatos1993@gmail.com>
2020-09-01 02:29:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-def name=(dog_name) #'name equals' method 
+def name=(dog_name) #'name equals' method - takes in an argument of a dog's name and sets that argument equal to a variable - this_dogs_name
   this_dogs_name = dog_name
 end
 
